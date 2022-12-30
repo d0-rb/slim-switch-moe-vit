a0a962f42b90cc88b423b9e74e746e4049aa5060
henry <hacperu2010@gmail.com>
2022-12-30 00:39:05 -0600

    added threshold stuff to argparser

--- d_cmd.sh
+++ d_cmd.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 read -p 'model: ' model #="deit_tiny_patch16_224"
 read -p 'cuda: ' cuda
+read -p 'num_cuda: ' num_cuda
 read -p 'seed: ' n #=0
 read -p 'starting threshold: ' start_threshold
 read -p 'target threshold: ' target_threshold
-CUDA_VISIBLE_DEVICES=$cuda python main.py --model $model --data-set CIFAR10 --data-path ./dataset --batch 128\
+CUDA_VISIBLE_DEVICES=$cuda python3 -m torch.distributed.launch --nproc_per_node=$num_cuda --master_port=$RANDOM --use_env main.py --model $model --data-set CIFAR10 --data-path ./dataset --batch 128\
                 --lr 1e-3 --epochs 300 --weight-decay 0.05 --sched cosine --input-size 224\
                 --eval-crop-ratio 1.0 --reprob 0.0 --smoothing 0.1 --warmup-epochs 5 --drop 0.0 \
                 --seed $n --opt adamw --warmup-lr 1e-6 --mixup .8 --drop-path 0.0 --cutmix 1.0 \
