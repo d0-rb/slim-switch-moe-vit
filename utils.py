8981d59b462bc077ceccd20d719c55746ed417d0
d0-rb <hacperu2010@gmail.com>
2023-01-05 23:53:53 -0600

    fixed visualization to use current thresh instead of tgt

--- utils.py
+++ utils.py
@@ -412,7 +412,7 @@ class TokenSkipVisualizer:
             # x.shape (B, Tokens, dim)
             B, T, D = x.shape
             
-            n = int(x.size(1) * gate.threshold)  # number of selected tokens
+            n = int(x.size(1) * gate._threshold)  # number of selected tokens
 
             # total_idx.shape (B, Tokens) [first n tokens along dim 1 are selected, rest are skip]
             total_idx = torch.arange(T, device='cpu').unsqueeze(0).repeat((B, 1))  # final idx mapping made by composing everything in indexes
