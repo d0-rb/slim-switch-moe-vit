8fc92cc8e5fc63a6063d1c14336f73df6ff779ef
Sandbox3aster <sandboxmaster@tamu.edu>
2023-01-17 21:11:58 -0800

    modify script and fix bugs.

--- Evo-ViT/run_evo_moe_imnet100.sh
+++ Evo-ViT/run_evo_moe_imnet100.sh
@@ -2,11 +2,13 @@
 
 MODEL=evo_moe_deit_tiny_patch16_224_expert8_sparse_init
 CUDA=6
-NUM_CUDA=4
+NUM_CUDA=1
 DATASET=IMNET100
 LR="5e-4"
 EPOCHS=300
+PORT=$((9000 + RANDOM % 1000))
 
-CUDA_VISIBLE_DEVICES=$CUDA main_deit.py --model ${MODEL} --data-set ${DATASET} \
+CUDA_VISIBLE_DEVICES=${CUDA} python -m torch.distributed.launch --nproc_per_node=${NUM_CUDA} --master_port=${PORT} --use_env \
+                           main_deit.py --model ${MODEL} --data-set ${DATASET} \
                            --drop-path 0 --batch-size 256 --data-path ~/DATASET/ImageNet100 --epochs ${EPOCHS} --lr ${LR}\
                            --output_dir logs/imnet100/${MODEL}/lr_${LR}_ep_${EPOCHS}
