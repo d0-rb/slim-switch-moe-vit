4c6ca94011041ae20bc791d82349a9010f5a53c0
scott <scotthoang1996@gmail.com>
2023-01-18 00:30:57 +0000

    added --unscale-lr back to cifar10/100 runs

--- scripts/run_resmoe_cifar10_ddp.sh
+++ scripts/run_resmoe_cifar10_ddp.sh
@@ -17,7 +17,7 @@ epochs=300
 
 CUDA_VISIBLE_DEVICES=$cuda python3 -m torch.distributed.launch --nproc_per_node=$num_cuda --master_port=$port --use_env main.py --model $model --data-set $dataset \
                 --data-path $datapath --batch $batchsize \
-                --lr $lr --epochs $epochs --weight-decay 0.05 --sched cosine --input-size 224 \
+                --lr $lr --unscale-lr --epochs $epochs --weight-decay 0.05 --sched cosine --input-size 224 \
                 --eval-crop-ratio 1.0 --reprob 0.0 --smoothing 0.1 --warmup-epochs 5 --drop 0.0 \
                 --seed $n --opt adamw --warmup-lr 1e-6 --mixup .8 --drop-path 0.0 --cutmix 1.0 \
                 --unscale-lr --no-repeated-aug --aa rand-m9-mstd0.5-inc1 \
