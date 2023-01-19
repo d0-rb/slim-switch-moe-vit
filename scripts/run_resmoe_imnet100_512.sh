ec4e71317b77650b6a0da8dcfedd63c2a2d2b4a6
Sandbox3aster <sandboxmaster@tamu.edu>
2023-01-19 10:42:14 -0800

    modify script

---
+++ scripts/run_resmoe_imnet100_512.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cuda=0
+batchsize=512
+model="resmoe_tiny_patch16_224_expert8_attn_loss_v4"
+lr="3e-4" #"3e-4"
+start_threshold="0.9"
+target_threshold="0.5"
+datapath="~/DATASET/ImageNet100"
+dataset="IMNET100"
+n=0  # seed
+epochs=300
+
+CUDA_VISIBLE_DEVICES=$cuda python main.py --model $model --data-set $dataset \
+                --data-path $datapath --batch $batchsize \
+                --lr $lr --unscale-lr --epochs $epochs --weight-decay 0.05 --sched cosine --input-size 224 \
+                --eval-crop-ratio 1.0 --reprob 0.0 --smoothing 0.1 --warmup-epochs 5 --drop 0.0 \
+                --seed $n --opt adamw --warmup-lr 1e-6 --mixup .8 --drop-path 0.0 --cutmix 1.0 \
+                --unscale-lr --no-repeated-aug --aa rand-m9-mstd0.5-inc1 \
+                --starting-threshold-dense $start_threshold --target-threshold-dense $target_threshold \
+                --starting-threshold-moe $start_threshold --target-threshold-moe $target_threshold \
+                --output_dir logs/${dataset}/${model}/scratch/lr_${lr}_ep_${epochs}_bs_${batchsize}/cosine/dense_${start_threshold}_${target_threshold}/moe_${start_threshold}_${target_threshold}/${n}
