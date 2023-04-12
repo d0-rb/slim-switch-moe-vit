e1621617210db9586998e5adb8c4603ff1be00c2
scott <scotthoang1996@gmail.com>
2023-04-12 02:58:16 +0000

    update training script

--- scripts/train_moe_imnet_ddp.sh
+++ scripts/train_moe_imnet_ddp.sh
@@ -2,9 +2,10 @@
 read -p 'cuda: ' cuda
 read -p 'data path: ' datapath
 read -p 'batch size: ' batchsize
-read -p 'gate: ' gate
-read -p 'num-experts: ' num_experts
+#read -p 'gate: ' gate
+#read -p 'num-experts: ' num_experts
 read -p 'epochs: ' epochs
+read -p 'model type: ' m_type
 
 num_comma=`echo ${cuda} | tr -cd , | wc -c`
 num_cuda=$((${num_comma} + 1))
@@ -12,23 +13,22 @@ echo $num_cuda
 echo $num_comma
 
 port=$((9000 + RANDOM % 1000))
-model="moe_tiny_patch16_224" #"moe_small_patch16_224" #"moe-base_patch16_224
+model="moe_${m_type}_patch16_224"
 lr="1e-3"
 start_threshold="0.5"
 dataset="IMNET"
+num_experts=32
 n=0  # seed
-validation_size=0.1
+gate='gshard'
+validation_size=0.001 # rougly 1k valid sample
 
 #CUDA_VISIBLE_DEVICES=$cuda python main.py --model $model --data-set $dataset\
 NCCL_P2P_DISABLE=1 CUDA_VISIBLE_DEVICES=$cuda torchrun --nproc_per_node=$num_cuda --master_port=$port main.py --model $model --data-set $dataset \
-		--unscale-lr \
-                --data-path $datapath --batch $batchsize \
-                --lr $lr --epochs $epochs --weight-decay 0.05 --sched cosine --input-size 224 \
-                --eval-crop-ratio 1.0 --reprob 0.0 --smoothing 0.1 --warmup-epochs 5 --drop 0.0 \
-                --seed $n --opt adamw --warmup-lr 1e-6 --mixup .8 --drop-path 0.0 --cutmix 1.0 \
-                --no-repeated-aug --aa rand-m9-mstd0.5-inc1 \
-		--num-experts $num_experts \
-                --gate $gate --pretrained \
-		--validation-size $validation_size \
-                --output_dir \
-		pretrained/${dataset}/${model}/${gate}/lr_${lr}_ep_${epochs}/experts_${num_experts}/val_${validation_size}/${n}\
+        --data-path $datapath --batch $batchsize \
+        --lr $lr --epochs $epochs --weight-decay 0.05 --sched cosine --input-size 224 \
+        --eval-crop-ratio 1.0 --reprob 0.0 --smoothing 0.1 --warmup-epochs 5 --drop 0.0 \
+        --seed $n --opt adamw --warmup-lr 1e-6 --mixup .8 --drop-path 0.0 --cutmix 1.0 \
+        --no-repeated-aug --aa rand-m9-mstd0.5-inc1 \
+	--num-experts $num_experts \
+        --gate $gate --pretrained \
+	--validation-size $validation_size \
